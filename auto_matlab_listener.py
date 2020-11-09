ac66437f1b680364a51cade0c8730b1c7b55142a
Caspar Gruijthuijsen <caspar.gr@gmail.com>
2020-11-10 00:30:15 +0100

    navigation via documentation hyperlinks added

--- auto_matlab_listener.py
+++ auto_matlab_listener.py
@@ -2,6 +2,7 @@ from os.path import join, abspath, split, isfile, getmtime
 import random
 import pickle
 import collections
+import re
 
 import sublime
 import sublime_plugin
@@ -35,6 +36,28 @@ def load_completions():
             with open(completions_path, 'br') as fh:
                 completions = pickle.load(fh)
 
+def create_hrefs(details):
+    # locate 'see also'
+    see_regex = re.compile(r'<p>see also:?\s*(.*?)\.?<\/p>', re.I)
+    mo_see = see_regex.search(details)
+
+    # extract referred functions
+    if mo_see:
+        hrefs_see = mo_see.group()
+        for parts in mo_see.group(1).split(','):
+            for ref in parts.split('<br>'):
+                ref = ref.strip()
+                # check if completions exist for function
+                if ref and completions.get(ref.lower()):
+                    # compose href for function
+                    href = '<a href="{}">{}</a>'.format(ref.lower(),ref)
+                    href_regex = r'\b'+ref+r'\b'
+                    hrefs_see = re.sub(href_regex, href, hrefs_see)
+        # replace referred function with href
+        details = details.replace(mo_see.group(),hrefs_see)
+        
+    return details
+
 
 class AutoMatlab(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
@@ -56,6 +79,7 @@ class AutoMatlab(sublime_plugin.EventListener):
             # read mfun from mfile to extract all data
             mfun_data = mfun(exact[2])
             if mfun_data.valid:
+                mfun_data.details = create_hrefs(mfun_data.details)
                 for i in range(len(mfun_data.defs)):
                     out.append([mfun_data.fun + '\t' + mfun_data.defs[i],
                                 mfun_data.snips[i]])
@@ -64,13 +88,21 @@ class AutoMatlab(sublime_plugin.EventListener):
                         easter[random.randrange(len(easter))]])
                 view.show_popup(mfun_data.details,
                                 sublime.COOPERATE_WITH_AUTO_COMPLETE,
-                                max_width=750, max_height=400)
+                                max_width=750, max_height=400,
+                                on_navigate=self.update_details_popup)
+                self.popup_view = view
         else:
             out = [[data[0] + '\t' + data[1], data[0]]
                    for fun, data in completions.items()
                    if fun.startswith(prefix)]
 
         return out
+
+    def update_details_popup(self, fun):
+        mfun_data = mfun(completions.get(fun)[2])
+        if mfun_data.valid:
+            mfun_data.details = create_hrefs(mfun_data.details)
+            self.popup_view.update_popup(mfun_data.details)
 
     def on_text_command(self, view, command_name, args):
         if not view.match_selector(view.sel()[0].a, 'source.matlab'):
